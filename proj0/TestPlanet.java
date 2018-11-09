5e42e2ed03bac0f23af0a3e3b1965d0e4ef9a962
ilovicki <ilovicki@163.com>
2018-11-09 22:55:52 +0800

    completed proj0

--- proj0/TestPlanet.java
+++ proj0/TestPlanet.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class TestPlanet {
+    private static void checkEquals(double actual, double expected, String label, double eps) {
+        if (Math.abs(expected - actual) <= eps * Math.max(expected, actual)) {
+            System.out.println("PASS: " + label + ": Expected " + expected + " and you gave " + actual);
+        } else {
+            System.out.println("FAIL: " + label + ": Expected " + expected + " and you gave " + actual);
+        }
+    }
+    private static void checkCalcForceExertedBy() {
+        System.out.println("Checking calcDistance...");
+
+        Planet p1 = new Planet(1.0, 1.0, 3.0, 4.0, 5.0, "jupiter.gif");
+        Planet p2 = new Planet(2.0, 1.0, 3.0, 4.0, 5.0, "jupiter.gif");
+
+        checkEquals(p1.calcForceExertedBy(p2), 25.0 * Planet.G, "calcForceExertedBy()", 0.01);
+        checkEquals(p2.calcForceExertedBy(p1), 25.0 * Planet.G, "calcForceExertedBy()", 0.01);
+    }
+    public static void main(String[] args) {
+        checkCalcForceExertedBy();
+    }
 }
