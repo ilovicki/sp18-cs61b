7975b6aebe914c8eaedf3055ea2a474bd7633af5
ilovicki <ilovicki@163.com>
2018-12-25 14:12:08 +0800

    proj1a style checked

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -1,5 +1,5 @@
 public class LinkedListDequeTest {
-	/* Utility method for printing out empty checks. */
+
 	public static boolean checkEmpty(boolean expected, boolean actual) {
 		if (expected != actual) {
 			System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
