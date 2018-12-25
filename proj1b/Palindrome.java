93a1aa27290f19edc00e08d2b1a513370ddd22b0
ilovicki <ilovicki@163.com>
2018-12-25 14:07:04 +0800

    style checked

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,7 +1,7 @@
 public class Palindrome {
     public Deque<Character> wordToDeque(String word) {
         Deque<Character> wordDeque = new ArrayDeque<>();
-        for(int i = 0; i < word.length(); i += 1) {
+        for (int i = 0; i < word.length(); i += 1) {
             wordDeque.addLast(word.charAt(i));
         }
         return wordDeque;
@@ -9,28 +9,28 @@ public class Palindrome {
 
     private String dequeToWord(Deque<Character> wordDeque) {
         String word = "";
-        while(!wordDeque.isEmpty()) {
+        while (!wordDeque.isEmpty()) {
             word += wordDeque.removeFirst();
         }
         return word;
     }
     public boolean isPalindrome(String word) {
         Deque<Character> wordDeque = wordToDeque(word);
-        if (wordDeque.isEmpty() || wordDeque.size() == 1){
+        if (wordDeque.isEmpty() || wordDeque.size() == 1) {
             return true;
         } else {
-            return wordDeque.removeFirst() == wordDeque.removeLast() && isPalindrome(dequeToWord(wordDeque));
+            return wordDeque.removeFirst() == wordDeque.removeLast()
+                    && isPalindrome(dequeToWord(wordDeque));
         }
     }
 
     public boolean isPalindrome(String word, CharacterComparator cc) {
         Deque<Character> wordDeque = wordToDeque(word);
-        if (wordDeque.isEmpty() || wordDeque.size() == 1){
+        if (wordDeque.isEmpty() || wordDeque.size() == 1) {
             return true;
         } else {
-            Character ch0 = wordDeque.removeFirst();
-            Character ch1 = wordDeque.removeLast();
-            return cc.equalChars(ch0, ch1) && isPalindrome(dequeToWord(wordDeque), cc);
+            return cc.equalChars(wordDeque.removeFirst(), wordDeque.removeLast())
+                    && isPalindrome(dequeToWord(wordDeque), cc);
         }
     }
 
